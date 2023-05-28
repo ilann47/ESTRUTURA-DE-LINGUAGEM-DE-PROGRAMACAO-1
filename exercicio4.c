fdef28943c2bed0488afaf3850491137cd8dee4a
ILAN WENDLING THOELE <41265766+ilann47@users.noreply.github.com>
2023-05-28 12:24:23 -0300

    Add files via upload

---
+++ exercicio4.c
@@ -0,0 +1,17 @@
+#include stdio.h
+#include math.h
+
+int main(void)
+/*ae(t)as(w)*/
+{
+ float t, w, y, x;
+ printf(entre com o valor de T );
+ scanf(%f, &t);
+ y = t2+4;
+ x = 2t-4;
+ w = 2t+4y-3x;
+ printf(o valor de W sera de %.2F, w);
+ 
+    
+return 0;
+}
