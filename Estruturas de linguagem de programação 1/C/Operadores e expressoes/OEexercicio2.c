943d2409dd36e209f0fcfde70e4572ee3139b5df
ILAN WENDLING THOELE <41265766+ilann47@users.noreply.github.com>
2023-06-14 14:41:10 -0300

    Add files via upload

---
+++ Estruturas de linguagem de programação 1/C/Operadores e expressoes/OEexercicio2.c
@@ -0,0 +1,28 @@
+/*
+2) Fornecido os valores da base e da altura de um triângulo retângulo, calcular a
+sua área e o seu perímetro. 
+*/
+
+
+
+
+
+#include <stdio.h>
+#include <math.h>
+
+
+/*ae(base, altura)as(area, perimetro)*/
+int main()
+{
+ 
+ float area, base, altura, hipotenusa, perimetro;
+ printf("digite o valor da base do triangulo retangulo ");
+ scanf("%F", &base);
+ printf("Digite o valor da altura do mesmo triangulo ");
+ scanf("%f", &altura);
+ area = base * altura / 2;
+ hipotenusa = sqrt(pow(altura,2) + pow(base,2));
+ perimetro = altura + base + hipotenusa;
+ printf("A area e %.2f O perimetro e %.2f", area, perimetro);
+ return 0;
+}
